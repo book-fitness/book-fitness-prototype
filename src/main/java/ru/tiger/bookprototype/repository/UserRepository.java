f86f2b2b97ea56b6a1491a43fe76a56eaf55fa34
Shekotov <pavel-shekotov@mail.ru>
2017-10-11 14:30:16 +0300

    Added comment

--- src/main/java/ru/tiger/bookprototype/repository/UserRepository.java
+++ src/main/java/ru/tiger/bookprototype/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    
+    //@Query("select u from User u where u.name = :username")
+    //User findByUsername(@Param("username") String username);
 }
