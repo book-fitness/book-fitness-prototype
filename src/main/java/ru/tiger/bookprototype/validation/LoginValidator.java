906c6d1618db5b00188a9e17194713ef624c7321
tiger <pavel-shekotov@mail.ru>
2020-12-05 20:34:02 +0300

    Registration

--- src/main/java/ru/tiger/bookprototype/validation/LoginValidator.java
+++ src/main/java/ru/tiger/bookprototype/validation/LoginValidator.java
@@ -6,6 +6,7 @@ package ru.tiger.bookprototype.validation;
  */
 public class LoginValidator extends AbstractValidator{
     public String getRegExp() {
-        return "[_A-Za-z0-9]{4,30}";
+        //return "[_A-Za-z0-9]{4,30}";
+        return "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     }
 }
--- src/main/java/ru/tiger/bookprototype/webservice/RegistrationWebService.java
+++ src/main/java/ru/tiger/bookprototype/webservice/RegistrationWebService.java
@@ -2,33 +2,43 @@ package ru.tiger.bookprototype.webservice;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import ru.tiger.bookprototype.security.web.DevServe;
 import ru.tiger.bookprototype.service.RegistrationService;
 
 /**
  *
  * @author Igor
  */
-
+@DevServe
 @Path("/registration")
 public class RegistrationWebService {
-   
+
     @Inject
     private RegistrationService registrationService;
-    
+
     @POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response tryRegister(RegistrationRequest request) {
-       try {
-           registrationService.tryToRegister(request.getLogin(), request.getPassword(), request.getRepeatPassword());
-           return Response.status(Response.Status.CREATED).build();
-       } catch(Exception ex) {
-           ex.printStackTrace();
-           return Response.serverError().build();
-       }
-   }
+        try {
+            System.out.println("Register tryRegister");
+            registrationService.tryToRegister(request.getLogin(), request.getPassword(), request.getRepeatPassword());
+            return Response.status(Response.Status.CREATED).build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @OPTIONS
+    @Path("/register")
+    public Response overviewPreflight() {
+        return Response
+                .ok().build();
+    }
 }
